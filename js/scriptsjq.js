54d9aa9aeb88a8bd6fda08d0cffe5b90ab6d7893
buxo <ablop4@gmail.com>
2022-02-04 13:37:11 +0100

    Cambios no menú

--- js/scriptsjq.js
+++ js/scriptsjq.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    alert("funciona");
+    abrirMenu();
+
+    //Abrir-Cerrar menu
+    function abrirMenu() {
+        //abrir
+        $(".interruptores #abrir").click(function(e) {
+            e.preventDefault();
+            $(this).slideUp(0);
+            $(".interruptores #cerrar").slideDown(500);
+            $("#menu").animate({ left: '15%' }, 1000);
+        });
+        //cerrar
+        $(".interruptores #cerrar").click(function(e) {
+            e.preventDefault();
+            $(this).slideUp(0);
+            $(".interruptores #abrir").slideDown(500);
+            $("#menu").animate({ left: '105%' }, 1000);
+        });
+    }
+
 });
